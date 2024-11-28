fdad17f1c7c77457f0cbce7d72535f9e969e08d2
Idza Ramaulkim <105291690+Rama0511@users.noreply.github.com>
2024-11-28 22:43:54 +0700

    Add files via upload

--- actions/actions.py
+++ actions/actions.py
@@ -55,9 +55,29 @@ class ActionProvideWeather(Action):
                 data = response.json()
                 temperature = data["main"]["temp"]
                 description = data["weather"][0]["description"]
+                plant = "none"
+                if temperature >= 0 and temperature < 5:
+                    plant = "Pine (Pinus), Spruce (Picea), Juniper (Juniperus), Holly (Ilex), Creeping Thyme (Thymus serpyllum)."
+                    
+                elif temperature >= 5 and temperature < 10:
+                    plant = "Cabbage (Brassica oleracea), Kale (Brassica oleracea var. sabellica), Spinach (Spinacia oleracea), Garlic (Allium sativum), Onion (Allium cepa)."
+                elif temperature >= 10 and temperature < 15:
+                    plant = "Lettuce (Lactuca sativa), Broccoli (Brassica oleracea var. italica), Peas (Pisum sativum), Radish (Raphanus sativus), Carrot (Daucus carota)."
+                elif temperature >= 15 and temperature < 20:
+                    plant = "Tomato (Solanum lycopersicum), Bell Pepper (Capsicum annuum), Eggplant (Solanum melongena), Strawberry (Fragaria × ananassa), Cucumber (Cucumis sativus)."
+                elif temperature >= 20 and temperature < 25:
+                    plant = "Basil (Ocimum basilicum), Oregano (Origanum vulgare), Mint (Mentha), Rosemary (Salvia rosmarinus), Thyme (Thymus vulgaris)."
+                elif temperature >= 25 and temperature < 30:
+                    plant = "Zinnia (Zinnia elegans), Marigold (Tagetes), Petunia (Petunia), Sunflower (Helianthus annuus), Cosmos (Cosmos bipinnatus)."
+                elif temperature >= 30:
+                    plant = "Cacti (Cactaceae), Succulents (various species), Bougainvillea (Bougainvillea glabra), Lantana (Lantana camara), Palms (Arecaceae)."
+                else:
+                    plant = "Pine (Pinus), Spruce (Picea), Juniper (Juniperus), Holly (Ilex), Creeping Thyme (Thymus serpyllum), Kale (Brassica oleracea var. sabellica), Winter Rye (Secale cereale), Garlic (Allium sativum), Carrot (Daucus carota), Snowdrop (Galanthus nivalis)."
+
                 response_message = (
                     f"It’s {temperature}°C currently in {location.title()}. "
                     f"The weather can be described as {description.capitalize()}."
+                    f"The plant that is suitable for the temperature of the city of {location.title()}, with a {temperature}°C temperature of  is the {plant}"
                 )
             else:
                 response_message = f"Sorry, I couldn't retrieve the weather for {location.title()}."
